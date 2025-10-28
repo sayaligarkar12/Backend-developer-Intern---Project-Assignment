ea1d1d4ee4921a5ad776219065192ae0aa7ec04b
Sayali Garkar <sayaligarkar@gmail.com>
2025-10-28 17:03:11 +0530

    Initialize Spring Boot application structure

---
+++ BackendAssignmentApploication.java
@@ -0,0 +1,11 @@
+package com.accolite.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAssignmentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BackendAssignmentApplication.class, args);
+    }
+}
