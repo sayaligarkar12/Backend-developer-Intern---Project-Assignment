99fa92fdfb1db2e1ac378a65781fe0854b32fcc7
Sayali Garkar <sayaligarkar@gmail.com>
2025-10-28 17:03:52 +0530

    Create User entity with JPA annotations

---
+++ user.java
@@ -0,0 +1,24 @@
+package com.accolite.assignment.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+    private String role; // "USER" or "ADMIN"
+}
